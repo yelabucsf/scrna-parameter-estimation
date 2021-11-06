7b57b1fdad2febd498f64a04b879d1710c106855
Min Cheol Kim <mincheoly@gmail.com>
2021-11-06 16:06:01 -0700

    updates

---
+++ analysis/perturb_thp1/preprocess/running_citeseqcount.sh
@@ -0,0 +1,40 @@
+nohup CITE-seq-Count -R1 SRR12075151_1.fastq.gz -R2 SRR12075151_2.fastq.gz -t GSM4633616_ECCITE_HTO_1-4_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run1.csv -o ./outs/hto1_out --expected_cells 10000 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075152_1.fastq.gz -R2 SRR12075152_2.fastq.gz -t GSM4633616_ECCITE_HTO_1-4_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run2.csv -o ./outs/hto2_out --expected_cells 10000 > csc.log &
+
+
+nohup CITE-seq-Count -R1 SRR12075153_1.fastq.gz -R2 SRR12075153_2.fastq.gz -t GSM4633616_ECCITE_HTO_1-4_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run3.csv -o ./outs/hto3_out --expected_cells 10000 > csc.log &
+
+
+nohup CITE-seq-Count -R1 SRR12075154_1.fastq.gz -R2 SRR12075154_2.fastq.gz -t GSM4633616_ECCITE_HTO_1-4_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run4.csv -o ./outs/hto4_out --expected_cells 10000 > csc.log &
+
+
+nohup CITE-seq-Count -R1 SRR12075155_1.fastq.gz -R2 SRR12075155_2.fastq.gz -t GSM4633617_ECCITE_HTO_5-8_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run5.csv -o ./outs/hto5_out --expected_cells 10000 > csc.log &
+
+
+nohup CITE-seq-Count -R1 SRR12075156_1.fastq.gz -R2 SRR12075156_2.fastq.gz -t GSM4633617_ECCITE_HTO_5-8_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run6.csv -o ./outs/hto6_out --expected_cells 10000 > csc.log &
+
+
+nohup CITE-seq-Count -R1 SRR12075157_1.fastq.gz -R2 SRR12075157_2.fastq.gz -t GSM4633617_ECCITE_HTO_5-8_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run7.csv -o ./outs/hto7_out --expected_cells 10000 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075158_1.fastq.gz -R2 SRR12075158_2.fastq.gz -t GSM4633617_ECCITE_HTO_5-8_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run8.csv -o ./outs/hto8_out --expected_cells 10000 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075159_1.fastq.gz -R2 SRR12075159_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run1.csv -o ./outs/gdo1_out --expected_cells 10000 --sliding-window --start-trim 16 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075160_1.fastq.gz -R2 SRR12075160_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run2.csv -o ./outs/gdo2_out --expected_cells 10000 --sliding-window --start-trim 16 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075161_1.fastq.gz -R2 SRR12075161_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run3.csv -o ./outs/gdo3_out --expected_cells 10000 --sliding-window --start-trim 16 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075162_1.fastq.gz -R2 SRR12075162_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run4.csv -o ./outs/gdo4_out --expected_cells 10000 --sliding-window --start-trim 16 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075163_1.fastq.gz -R2 SRR12075163_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run5.csv -o ./outs/gdo5_out --expected_cells 10000 --sliding-window --start-trim 16 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075164_1.fastq.gz -R2 SRR12075164_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run6.csv -o ./outs/gdo6_out --expected_cells 10000 --sliding-window --start-trim 16 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075165_1.fastq.gz -R2 SRR12075165_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run7.csv -o ./outs/gdo7_out --expected_cells 10000 --sliding-window --start-trim 16 > csc.log &
+
+nohup CITE-seq-Count -R1 SRR12075166_1.fastq.gz -R2 SRR12075166_2.fastq.gz -t GSM4633618_ECCITE_GDO_Barcodes.csv -cbf 1 -cbl 16 -umif 17 -umil 26 -wl ../cell_bcs/run8.csv -o ./outs/gdo8_out --expected_cells 10000 --sliding-window --start-trim 16 > csc8.log &
+
+
+
+
