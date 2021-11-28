218ef38b87f8d777d5abcb04913212cbcb21ecb1
Min Cheol Kim <mincheoly@gmail.com>
2021-11-28 15:04:06 -0800

    work

--- memento/estimator.py
+++ memento/estimator.py
@@ -271,6 +271,7 @@ def _corr_from_cov(cov, var_1, var_2, boot=False):
 		
 	corr[np.isfinite(var_prod)] = cov[np.isfinite(var_prod)] / var_prod[np.isfinite(var_prod)]
 	
-	corr[(corr > 1) | (corr < -1)] = np.nan
+	corr[corr > 1] = 1
+	corr[corr < -1] = -1
 	
 	return corr
