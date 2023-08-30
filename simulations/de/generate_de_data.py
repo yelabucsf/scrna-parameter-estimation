3533f292671c7c7ec68ae3b54e9631e481f227eb
mincheoly <mincheoly@gmail.com>
2023-08-30 16:59:35 -0700

    finalize de

--- simulations/de/generate_de_data.py
+++ simulations/de/generate_de_data.py
@@ -1,3 +1,7 @@
+# Generate DE data
+
+#Generate DE data with 2 replicates
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import scanpy as sc
@@ -22,15 +26,55 @@ import memento
 import memento.simulate as simulate
 
 data_path = '/data_volume/memento/simulation/'
-num_replicates = 2
 
 
-def get_random_string(length):
-    # choose from all lowercase letter
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return result_str
+### Setup simulation
 
+ifn_adata = sc.read('/data_volume/memento/hbec/' + 'HBEC_type_I_filtered_counts_deep.h5ad')
+q=0.07
+
+adata_1 = ifn_adata[(ifn_adata.obs['cell_type'] == 'ciliated') & (ifn_adata.obs['stim'] == 'control')]
+adata_2 = ifn_adata[(ifn_adata.obs['cell_type'] == 'ciliated') & (ifn_adata.obs['stim'] == 'beta')]
+
+x_param_1, z_param_1, Nc_1, good_idx_1 = simulate.extract_parameters(adata_1.X, q=q)
+x_param_2, z_param_2, Nc_2, good_idx_2 = simulate.extract_parameters(adata_2.X, q=q)
+common_set = np.array(list(set(good_idx_1) & set(good_idx_2)))
+x_param_1 = (
+    np.array([x for x,i in zip(x_param_1[0], good_idx_1) if i in common_set]),
+    np.array([x for x,i in zip(x_param_1[1], good_idx_1) if i in common_set]))
+x_param_2 = (
+    np.array([x for x,i in zip(x_param_2[0], good_idx_2) if i in common_set]),
+    np.array([x for x,i in zip(x_param_2[1], good_idx_2) if i in common_set]))
+z_param_1 = (
+    np.array([x for x,i in zip(z_param_1[0], good_idx_1) if i in common_set]),
+    np.array([x for x,i in zip(z_param_1[1], good_idx_1) if i in common_set]))
+z_param_2 = (
+    np.array([x for x,i in zip(z_param_2[0], good_idx_2) if i in common_set]),
+    np.array([x for x,i in zip(z_param_2[1], good_idx_2) if i in common_set]))
+
+pos_var_condition = (z_param_1[1] > 0) & (z_param_2[1] > 0)
+x_param_1 = (x_param_1[0][pos_var_condition], x_param_1[1][pos_var_condition])
+x_param_2 = (x_param_2[0][pos_var_condition], x_param_2[1][pos_var_condition])
+z_param_1 = (z_param_1[0][pos_var_condition], z_param_1[1][pos_var_condition])
+z_param_2 = (z_param_2[0][pos_var_condition], z_param_2[1][pos_var_condition])
+
+d1 = (z_param_1[1] - z_param_1[0])/z_param_1[0]**2
+d2 = (z_param_2[1] - z_param_2[0])/z_param_2[0]**2
+d1[d1 < 0] = 1e-3
+d2[d2 < 0] = 1e-3
+
+estimated_TE = np.log(x_param_2[0]) - np.log(x_param_1[0])
+estimated_TE[np.absolute(estimated_TE) < 0.1] = 0
+available_de_idxs = np.where(np.absolute(estimated_TE) > 0)[0]
+
+num_genes = x_param_1[0].shape[0]
+base_mean = np.log(x_param_1[0])
+treatment_effects = np.zeros(num_genes)
+num_de = 2000
+de_idxs = np.random.choice(available_de_idxs, num_de)
+treatment_effects[de_idxs] =  estimated_TE[de_idxs]
+
+### Generate
 
 def convert_params_nb(mu, theta):
     """
@@ -43,131 +87,72 @@ def convert_params_nb(mu, theta):
     p = (var - mu) / var
     return r, 1 - p
 
+num_replicates = 2
+cell_counts = np.array([1000, 1000, 1100, 1100])
 
-def convert_params_binom(mu, v):
-    """
-    Convert mean/dispersion parameterization of a negative binomial to the ones scipy supports
+num_cells = cell_counts.sum()
+expr_matrix = np.zeros((num_cells, num_genes))
+ind1_intercepts = base_mean
+ind2_intercepts = base_mean
+delta_te = stats.norm.rvs(scale=0.5, size=num_genes)
 
-    See https://en.wikipedia.org/wiki/Negative_binomial_distribution#Alternative_formulations
-    """
-    p = 1-(v/mu)
-    n = mu/p
+relative_means = np.exp(np.vstack([
+    ind1_intercepts,
+    ind1_intercepts + treatment_effects + delta_te/2,
+    ind2_intercepts,
+    ind2_intercepts + treatment_effects - delta_te/2]))
 
-    return np.ceil(n).astype(int), p
+for i in range(num_genes):
 
+    gene_expr = []
+    for rep_idx in range(num_replicates*2):
+        mean = relative_means[rep_idx, i]*np.random.choice(Nc_1, cell_counts[rep_idx])
+        cells = stats.nbinom.rvs(*convert_params_nb(mean, np.mean(d1)), size=cell_counts[rep_idx])
+        gene_expr.append(cells)
+    expr_matrix[:, i] = np.concatenate(gene_expr)
+   
 
-def simulate_counts(means, dispersions, num_cells):
-    
-    num_groups, num_genes = means.shape
-    
-    counts = []
-    for group_idx in range(num_groups):
-        counts.append(
-            stats.nbinom.rvs(*convert_params_nb(means[group_idx], 1/dispersions[group_idx]), size=(num_cells[group_idx], num_genes))
-        )
-    
-    counts = np.vstack(counts)
+### Perform sampling
 
-    return counts
+_, expr_matrix = simulate.capture_sampling(expr_matrix.astype(int), q=q, process='hyper')
 
+### Generate the dataframes
 
-if __name__ == '__main__':
-    ifn_adata = sc.read('/data_volume/memento/hbec/' + 'HBEC_type_I_filtered_counts_deep.h5ad')
-    q=0.07
-
-    adata_1 = ifn_adata[(ifn_adata.obs['cell_type'] == 'ciliated') & (ifn_adata.obs['stim'] == 'control')]
-    adata_2 = ifn_adata[(ifn_adata.obs['cell_type'] == 'ciliated') & (ifn_adata.obs['stim'] == 'beta')]
-
-    x_param_1, z_param_1, Nc_1, good_idx_1 = simulate.extract_parameters(adata_1.X, q=q)
-    x_param_2, z_param_2, Nc_2, good_idx_2 = simulate.extract_parameters(adata_2.X, q=q)
-    common_set = np.array(list(set(good_idx_1) & set(good_idx_2)))
-    x_param_1 = (
-        np.array([x for x,i in zip(x_param_1[0], good_idx_1) if i in common_set]),
-        np.array([x for x,i in zip(x_param_1[1], good_idx_1) if i in common_set]))
-    x_param_2 = (
-        np.array([x for x,i in zip(x_param_2[0], good_idx_2) if i in common_set]),
-        np.array([x for x,i in zip(x_param_2[1], good_idx_2) if i in common_set]))
-
-    pos_var_condition = (x_param_1[1] > 0) & (x_param_2[1] > 0)
-    x_param_1 = (x_param_1[0][pos_var_condition], x_param_1[1][pos_var_condition])
-    x_param_2 = (x_param_2[0][pos_var_condition], x_param_2[1][pos_var_condition])
-
-    estimated_TE = np.log(x_param_2[0]) - np.log(x_param_1[0])
-    estimated_TE = stats.norm.rvs(scale=1, size=x_param_2[0].shape[0])
-    estimated_TE[np.absolute(estimated_TE) < 0.25] = 0
-    available_de_idxs = np.where(np.absolute(estimated_TE) > 0)[0]
-
-    num_genes = x_param_1[0].shape[0]
-    treatment_effect = np.zeros(num_genes)
-    num_de = 2000
-    de_idxs = np.random.choice(available_de_idxs, num_de)
-    treatment_effect[de_idxs] = stats.norm.rvs(size=num_de)     #estimated_TE[de_idxs]
-
-    conditions = ['ctrl', 'stim']
-    groups = [get_random_string(5) for i in range(num_replicates)]
-    df = pd.DataFrame(
-        itertools.product(groups, conditions),
+group = \
+    ['A' for i in range(cell_counts[0])] + \
+    ['A' for i in range(cell_counts[1])] + \
+    ['B' for i in range(cell_counts[2])] + \
+    ['B' for i in range(cell_counts[3])]
+condition = \
+    ['ctrl' for i in range(cell_counts[0])] + \
+    ['stim' for i in range(cell_counts[1])] + \
+    ['ctrl' for i in range(cell_counts[2])] + \
+    ['stim' for i in range(cell_counts[3])]
+obs = pd.DataFrame(
+        zip(group, condition), 
+        index=['cell'+str(i) for i in range(num_cells)],
         columns=['group', 'condition'])
+var = pd.DataFrame(index=['gene'+str(i) for i in range(num_genes)])
 
-    cov_df = pd.get_dummies(df[['group']], drop_first=True).astype(float)
-    cov_df -= cov_df.mean()
-    stim_df = (df[['condition']]=='stim').astype(float)
-    interaction_df = cov_df*stim_df[['condition']].values
-    interaction_df.columns=[f'interaction_{col}' for col in cov_df.columns]
-    cov_df = pd.concat([cov_df, interaction_df], axis=1)
-    cov_df = sm.add_constant(cov_df)
-    design = pd.concat([cov_df, stim_df], axis=1)
+### Save AnnData
 
-    base_mean = np.log(x_param_1[0])
+anndata = sc.AnnData(
+    sp.sparse.csr_matrix(expr_matrix, dtype=float), 
+    obs=obs, 
+    var=var)
+de_genes = np.zeros(num_genes)
+de_genes[de_idxs] = 1
+anndata.var['is_de'] = de_genes.astype(bool)
+anndata.var['treatment_effect'] = treatment_effects
 
-    mean_beta = np.vstack([
-        np.log(x_param_1[0]),
-        np.vstack([stats.norm.rvs(scale=0.0, size=num_genes) for i in range((num_replicates-1))]), # intercept random effect
-        np.vstack([stats.norm.rvs(scale=1.0, size=num_genes) for i in range((num_replicates-1))]), # treatment random effect
-        treatment_effect])
+anndata.write(data_path + 'de/anndata.h5ad')
 
-    means = np.exp(design.values@mean_beta)
+norm_adata = anndata.copy().copy()
+sc.pp.normalize_total(norm_adata, target_sum=1)
+norm_adata.write(data_path + 'de/norm_anndata.h5ad')
 
-    dispersions = stats.uniform.rvs(0.1, 1, size=(num_replicates*2, num_genes))
-
-    cell_sizes = np.random.choice(Nc_1, num_replicates*2).reshape(-1,1)
-    cell_sizes = (np.ones(num_replicates*2)*20000).reshape(-1,1)
-    print(df)
-    print(design)
-    print(means[:, de_idxs])
-
-    num_cells = np.array([1000, 1000, 1000, 1000])
-    design = df
-    # design = pd.concat([design for i in range(num_cells_per_group)])
-    design = design.loc[design.index.repeat(num_cells)]
-    
-    counts = simulate_counts(means*cell_sizes, dispersions, num_cells).astype(int)
-    
-    cell_names = [f'cell_{i}' for i in range(counts.shape[0])]
-    gene_names = [f'gene_{i}' for i in range(counts.shape[1])]
-    design.index=cell_names
-    
-    _, hyper_captured = simulate.capture_sampling(counts, q=q, process='hyper')
-
-    expr_df = pd.DataFrame(hyper_captured, index=cell_names, columns=gene_names)
-    grouped = pd.concat([design, expr_df], axis=1)
-    pseudobulks = grouped.groupby(['group', 'condition'])[gene_names].sum()
-    pseudobulks.index = [x+'-'+y for x, y in itertools.product(design['group'].unique(), design['condition'].unique())]
-
-    anndata = sc.AnnData(
-        sp.sparse.csr_matrix(hyper_captured), 
-        obs=design, 
-        var=pd.DataFrame(index=gene_names))
-    de_genes = np.zeros(num_genes)
-    de_genes[de_idxs] = 1
-    anndata.var['is_de'] = de_genes.astype(bool)
-    anndata.var['treatment_effect'] = treatment_effect
-
-    anndata.write(data_path + 'de/anndata.h5ad')
-    pseudobulks.T.to_csv(data_path + 'de/pseudobulks.csv')
-    
-    norm_adata = anndata.copy().copy()
-    sc.pp.normalize_total(norm_adata, target_sum=1)
-    norm_adata.write(data_path + 'de/norm_anndata.h5ad')
-    
-    print('data generation done')
+expr_df = pd.DataFrame(expr_matrix, index=obs.index, columns=var.index)
+grouped = pd.concat([obs, expr_df], axis=1)
+pseudobulks = grouped.groupby(['group', 'condition'])[expr_df.columns].sum()
+pseudobulks.index = [x+'_'+y for x,y in pseudobulks.index]
+pseudobulks.T.to_csv(data_path + 'de/pseudobulks.csv')
