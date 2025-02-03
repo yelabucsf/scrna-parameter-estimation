60c94d81f8c0aa38e6ebc4a6b607fdf4de9f0c75
Min Cheol Kim <mincheol@Helens-MacBook-Air-2.local>
2025-02-03 10:52:15 -0800

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='memento-de',
-    version='0.1.1',
+    version='0.1.2',
     description='Hypothesis testing for scRNA-seq',
     url='https://github.com/yelabucsf/scrna-parameter-estimation.git',
     author='Min Cheol Kim',
